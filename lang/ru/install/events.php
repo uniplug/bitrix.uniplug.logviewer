73f37f5e3ee42d821993ad86ab9036ecd1510464
Viktor <viktor@uniplug.ru>
2015-06-04 14:04:08 +0300

    Init commit

---
+++ lang/ru/install/events.php
@@ -0,0 +1,6 @@
+<?
+$MESS['UNIPLUG_REVIEWS_MODULE_EVENT_NAME_MODERATOR']		= 'Добавление отзыва';
+$MESS['UNIPLUG_REVIEWS_MODULE_EVENT_DESCRIPTION_MODERATOR']	= 'При добавлении отзыва отправляется уведомление на электронный ящик для модерации';
+$MESS['UNIPLUG_REVIEWS_MODULE_SUBJECT_MODERATOR']			= '#SITE_NAME#: Клиентом добавлен отзыв';
+$MESS['UNIPLUG_REVIEWS_MODULE_MESSAGE_MODERATOR']			= "<p>Информационное сообщение сайта #SITE_NAME#</p>\n<p>---------------------------------------------------------------------</p>\n<p><b>Добавлен отзыв</b></p>\n<p>Имя клиента: #CLIENT_NAME# <#CLIENT_EMAIL#></p>\n<p>#CLIENT_TEXT#</p>\n<p><a href=\"http://#SERVER_NAME##URL#\">Адрес записи</a></p>\n<p>---------------------------------------------------------------------</p>\n<p>Сообщение сгенерировано автоматически.</p>";
+?>
